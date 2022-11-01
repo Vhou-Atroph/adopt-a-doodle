d93c0b65321bfa02605d31f78efd8723659491c2
Vhou-Atroph <79807240+Vhou-Atroph@users.noreply.github.com>
2022-11-01 00:02:09 -0500

    Update lib.rs

--- src/rustydoodle/src/lib.rs
+++ src/rustydoodle/src/lib.rs
@@ -5,6 +5,8 @@ use pyo3::prelude::*;
 struct Doodle {
     #[pyo3(get,set)]
     pattern: Pattern,
+    #[pyo3(get,set)]
+    animation: Option<Animation>,
 }
 
 /// A pattern that a Doodle may have.
@@ -17,4 +19,24 @@ struct Pattern {
     legs: Option<String>, // Some doodle patterns affect more than just the body of the doodle- having an Option here will handle those.
     #[pyo3(get,set)]
     tail: Option<String>
+}
+
+/// An animation that the doodle will perform. 
+#[pyclass]
+#[derive(Clone)]
+struct Animation {
+    #[pyo3(get,set)]
+    file: String,
+    #[pyo3(get,set)]
+    anim_loop: bool,
+    #[pyo3(get,set)]
+    loop_from: Option<u64>,
+    #[pyo3(get,set)]
+    loop_to: Option<u64>,
+    #[pyo3(get,set)]
+    loop_restart: Option<u64>,
+    #[pyo3(get,set)]
+    pose: bool,
+    #[pyo3(get,set)]
+    pose_frame: Option<u64>
 }
