6636f21346a62a86805cf746c3f0d0976c0f8905
Vhou-Atroph <79807240+Vhou-Atroph@users.noreply.github.com>
2022-11-02 19:27:17 -0500

    Update lib.rs
    position may not actually be very necessary to have in the base doodle struct- for flexibility it's likely more efficient to have position be changed in python itself.

--- src/rustydoodle/src/lib.rs
+++ src/rustydoodle/src/lib.rs
@@ -11,8 +11,6 @@ struct Doodle {
     pattern: Pattern,
     #[pyo3(get,set)]
     animation: Option<Animation>,
-    #[pyo3(get,set)]
-    position: (i64,i64,i64) // Tuple of values to get the doodle's position in a 3D space.
 }
 
 /// A Color tuple for Panda3D. It is in the format of (Red,Green,Blue,Alpha)
