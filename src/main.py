1a017b735c26c1d9275d1af0c5e99d81a7cf494e
Vhou-Atroph <79807240+Vhou-Atroph@users.noreply.github.com>
2022-11-03 21:53:23 -0500

    Update main.py

--- src/main.py
+++ src/main.py
@@ -4,4 +4,8 @@ import rustydoodle
 
 def adopt(actor:rustydoodle.Doodle) -> Actor:
     doodle = Actor("TT_pets-mod.bam",{"animation":actor.animation.file})
+
+    doodle.play("animation")
+    if actor.animation.anim_loop:
+        doodle.loop("animation",restart=actor.animation.loop_restart,fromFrame=actor.animation.loop_from,toFrame=actor.animation.loop_to)
     return doodle
