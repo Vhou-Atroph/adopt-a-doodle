358030822a3245a6a3186f1ae376f78bec5269b5
Vhou-Atroph <79807240+Vhou-Atroph@users.noreply.github.com>
2022-11-04 00:14:35 -0500

    Update adopt.py

--- src/adopt_a_doodle_vhou-atroph/adopt.py
+++ src/adopt_a_doodle_vhou-atroph/adopt.py
@@ -6,6 +6,22 @@ def adopt(actor:rustydoodle.Doodle) -> Actor:
     """Creates a Doodle actor."""
     doodle = Actor("TT_pets-mod.bam",{"animation":actor.animation.file})
 
+    for i in rustydoodle.hair_list():
+        if i != actor.hair:
+            doodle.findAllMatches('**/'+i).hide()
+    
+    for i in rustydoodle.ear_list():
+        if i != actor.ears:
+            doodle.findAllMatches('**/'+i).hide()
+    
+    for i in rustydoodle.nose_list():
+        if i != actor.nose:
+            doodle.findAllMatches('**/'+i).hide()
+    
+    for i in rustydoodle.tail_list():
+        if i != actor.tail:
+            doodle.findAllMatches('**/'+i).hide()
+
     doodle.play("animation")
     if actor.animation.anim_loop:
         doodle.loop("animation",restart=actor.animation.loop_restart,fromFrame=actor.animation.loop_from,toFrame=actor.animation.loop_to)
